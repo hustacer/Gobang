23a895303c7d011d1aaeba371814ecdcbf63f62c
hustacer <xiaobinly@gmail.com>
2019-03-29 16:10:04 +0900

    status bar update for restart

--- app/src/main/java/com/example/fnst/gobang/MainActivity.java
+++ app/src/main/java/com/example/fnst/gobang/MainActivity.java
@@ -1,26 +1,31 @@
 package com.example.fnst.gobang;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private GobangPanel gobangPanel;
-    private Button restart;
+    private ImageView restartImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
 
         gobangPanel = (GobangPanel) findViewById(R.id.id_gobang);
-        restart = (Button)findViewById(R.id.restart);
+        restartImageView = (ImageView)findViewById(R.id.restart_click);
 
-        restart.setOnClickListener(new View.OnClickListener() {
+        restartImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 gobangPanel.reStart();
-            }
+        }
         });
     }
 }
